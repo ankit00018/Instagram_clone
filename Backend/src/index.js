70ae1cc3d944ee395e7d0feed6a3ed077cf960ff
ankit00018 <ankitkachhad9@gmail.com>
2025-03-06 16:03:23 +0530

    Instagram Clone setup, and database Model

---
+++ Backend/src/index.js
@@ -0,0 +1,21 @@
+import express, {urlencoded} from "express"
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+import { app } from "./app.js"
+
+
+dotenv.config({
+    path:'./.env'
+})
+
+
+connectDB()
+.then( ()=>{
+    app.listen(process.env.PORT || 80000, ()=>{
+        console.log(`Server is running on ${process.env.PORT}`);
+    })
+} )
+
+.catch( (error)=>{
+    console.log("MongoDB Connection Failed",error);
+} )
