9cf5f1aeff203ec8572bbad92fa892f1d580913e
ankit00018 <ankitkachhad9@gmail.com>
2025-03-08 21:28:17 +0530

    Add comment management and post deletion functionality to post controller

--- Backend/src/controllers/post.controller.js
+++ Backend/src/controllers/post.controller.js
@@ -4,6 +4,7 @@ import sharp from "sharp";
 import { Post } from "../models/post.model.js";
 import { User } from "../models/user.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { Comment } from "../models/comment.model.js";
 
 const addNewPost = asyncHandler(async (req, res) => {
   const caption = req.body;
@@ -135,7 +136,74 @@ const addComment = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, comment, "Comment published"));
 });
 
+const getCommentsOfPost = asyncHandler(async (req, res) => {
+  const postId = req.params.id;
 
+  const comments = await Comment.find({ post: postId }).populate(
+    "author",
+    "username profilePicture"
+  );
+
+  if (!comments) {
+    throw new ApiErrors(401, "Unauthorized request");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(201, comments, "All comments fetched"));
+});
+
+const deletePost = asyncHandler(async (req, res) => {
+  const postId = req.params.id;
+  const authorId = req.id;
+
+  const post = await Post.findById(postId);
+  if (!post) {
+    throw new ApiErrors(401, "Post not found");
+  }
+
+  // check if the logged-in user is the owner of the post
+  if (post.author.toString() !== authorId) {
+    throw new ApiErrors(400, "Unauthorized request");
+  }
+
+  // delete post
+  await Post.findByIdAndDelete(postId);
+
+  // remove the post id from the user's post
+  let user = await User.findById(authorId);
+  user.post = user.post.filter((id) => id.toString() !== postId);
+  await user.save();
+
+  // delete associated comments
+  await Comment.deleteMany({ post: postId });
+
+  return res.status(200).json(new ApiResponse(201, null, "Post deleted"));
+});
+
+const savedPost = asyncHandler(async (req, res) => {
+  const postId = req.params.id;
+  const authorId = req.id;
+  const post = await Post.findById(postId);
+  if (!post) {
+    throw new ApiErrors(400, "Post not found");
+  }
+
+  const user = await User.findById(authorId);
+  if (user.bookmarks.includes(post._id)) {
+    // already bookmarked -> remove from the bookmark
+    await user.updateOne({ $pull: { bookmarks: post._id } });
+    await user.save();
+    return res
+      .status(200)
+      .json(new ApiResponse(201, null, "Post removed from saved"));
+  } else {
+    // bookmark krna pdega
+    await user.updateOne({ $addToSet: { bookmarks: post._id } });
+    await user.save();
+    return res.status(200).json(new ApiResponse(201, null, "Post saved"));
+  }
+});
 
 export {
   addNewPost,
@@ -144,4 +212,7 @@ export {
   likePost,
   disLikePost,
   addComment,
+  getCommentsOfPost,
+  deletePost,
+  savedPost,
 };
