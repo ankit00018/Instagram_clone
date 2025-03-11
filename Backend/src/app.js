bd35cb6d5b5694e660720a5fe8ac7fd2695793bd
ankit00018 <ankitkachhad9@gmail.com>
2025-03-12 00:32:58 +0530

    Initialize frontend with React and Vite setup, add utility functions, and configure TypeScript and ESLint

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,9 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
+    origin:process.env.CORS_ORIGIN || "http://localhost:5173", 
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials:true
 }))
 
