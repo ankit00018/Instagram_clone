c95c644f1d14b1c393bf4a8fc76071fbdc78f744
ankit00018 <ankitkachhad9@gmail.com>
2025-03-20 23:20:55 +0530

    Add Feed, Posts, and RightSidebar components; update Home and LeftSidebar for layout integration

--- Frontend/src/components/LeftSidebar.jsx
+++ Frontend/src/components/LeftSidebar.jsx
@@ -56,7 +56,7 @@ const LeftSidebar = () => {
     return (
         <div className="fixed top-0 z-10 left-0 px-4 border-r border-gray-400 w-[16%] h-screen">
             <div className="flex flex-col">
-                <h1>LOGO</h1>
+                <h1 className="my-8 pl-3 font-bold text-xl">LOGO</h1>
                 <div>
             {
                  sidebarIcons.map((item,index) =>{
